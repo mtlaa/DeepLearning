08d237487cbd5f9e554532809589c8ecd45d8583
mtlaa <1921283015@qq.com>
2023-03-03 18:05:44 +0800

    完成L1W3

--- L1W3.py
+++ L1W3.py
@@ -8,6 +8,153 @@ from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, l
 
 np.random.seed(1)  # 固定随机数种子，使每次随机初始化的参数都一样
 
+
+def layer_sizes(X, Y):
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    return n_x, n_h, n_y
+
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(2)
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b1 = np.zeros((n_h, 1))
+    b2 = np.zeros((n_y, 1))
+    parameters = {"W1": W1,
+                  "W2": W2,
+                  "b1": b1,
+                  "b2": b2}
+    return parameters
+
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    b1 = parameters["b1"]
+    b2 = parameters["b2"]
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2": Z2,
+             "A2": A2}
+    return A2, cache
+
+
+def compute_cost(A2, Y, parameters):
+    m = Y.shape[1]
+    cost = (-1 / m) * np.sum(np.log(A2) * Y + (1 - Y) * np.log((1 - A2)))
+    cost = np.squeeze(cost)
+    return cost
+
+
+def backward_propagation(parameters, cache, X, Y):
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    m = Y.shape[1]
+
+    dZ2 = A2 - Y
+    dW2 = np.dot(dZ2, A1.T) / m
+    db2 = np.sum(dZ2, axis=1, keepdims=True) / m
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = np.dot(dZ1, X.T) / m
+    db1 = np.sum(dZ1, axis=1, keepdims=True) / m
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    b1 = parameters["b1"]
+    b2 = parameters["b2"]
+    dW1 = grads["dW1"]
+    dW2 = grads["dW2"]
+    db1 = grads["db1"]
+    db2 = grads["db2"]
+    W1 = W1 - learning_rate * dW1
+    W2 = W2 - learning_rate * dW2
+    b1 = b1 - learning_rate * db1
+    b2 = b2 - learning_rate * db2
+    parameters = {"W1": W1,
+                  "W2": W2,
+                  "b1": b1,
+                  "b2": b2}
+    return parameters
+
+
+def nn_model(X, Y, n_h, num_iterations=10000, print_cost=False):
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    for i in range(num_iterations):
+        A2, cache = forward_propagation(X, parameters)
+        cost = compute_cost(A2, Y, parameters)
+        grads = backward_propagation(parameters, cache, X, Y)
+        parameters = update_parameters(parameters, grads)
+        if i % 100 == 0 and print_cost:
+            print("Cost after iteration %i: %f" % (i, cost))
+    return parameters
+
+
+def predict(parameters, X):
+    A2, cache = forward_propagation(X, parameters)
+    return np.round(A2)
+
 X, Y = load_planar_dataset()
-plt.scatter(X[0, :], X[1, :], c=Y.reshape(X[0,:].shape), s=40, cmap=plt.cm.Spectral)
+# plt.scatter(X[0, :], X[1, :], c=Y.reshape(X[0, :].shape), s=40, cmap=plt.cm.Spectral)
+# plt.show()
+# print(X.shape)
+# print(Y.shape)
+
+# 4个 hidden layer
+# # Build a model with a n_h-dimensional hidden layer
+# parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
+# # Plot the decision boundary
+# plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+# plt.title("Decision Boundary for hidden layer size " + str(4))
+# plt.show()
+# # Print accuracy
+# predictions = predict(parameters, X)
+# print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
+
+
+# LogisticRegression
+# clf = sklearn.linear_model.LogisticRegressionCV()
+# clf.fit(X.T, Y.T)
+# # Plot the decision boundary for logistic regression
+# plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+# plt.title("Logistic Regression")
+# plt.show()
+# # Print accuracy
+# LR_predictions = clf.predict(X.T)
+# print ('Accuracy of logistic regression: %d ' % float((np.dot(Y,LR_predictions) + np.dot(1-Y,1-LR_predictions))/float(Y.size)*100) +
+#        '% ' + "(percentage of correctly labelled datapoints)")
+
+# This may take about 2 minutes to run
+
+plt.figure(figsize=(16, 32))
+hidden_layer_sizes = [1, 2, 3, 4, 5, 10, 20]
+for i, n_h in enumerate(hidden_layer_sizes):
+    plt.subplot(5, 2, i+1)
+    plt.title('Hidden Layer of size %d' % n_h)
+    parameters = nn_model(X, Y, n_h, num_iterations = 5000)
+    plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+    predictions = predict(parameters, X)
+    accuracy = float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100)
+    print ("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
 plt.show()
